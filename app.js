36be2fb850a7e0d726586714f5acbf4566d75902
Kamil Kowalczyk <kovval78@gmail.com>
2021-03-12 16:50:34 +0100

    Replace element <li> in component Item

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const Item = (props) => <li>{`Test: ${props.arrayElement}`}</li>;
+
 class ListItems extends React.Component {
   state = {
     items: ["item-1", "item-2", "item-3", "item-4", "item-5"],
   };
   render() {
+    const Items = this.state.items.map((item) => (
+      <Item key={item} arrayElement={item} />
+    ));
     return (
       <ul>
-        {this.state.items.map((item) => (
-          <li key={item}>{`Test: ${item}`}</li>
-        ))}
+        {/* Component instead of <li> element     */}
+        {Items}
       </ul>
     );
   }
